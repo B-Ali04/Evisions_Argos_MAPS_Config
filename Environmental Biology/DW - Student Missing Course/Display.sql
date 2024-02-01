a0c9dc090ae2f85709dccaefd37f7517ef5fde2a
B-Ali04 <86619743+B-Ali04@users.noreply.github.com>
2024-02-01 08:55:41 -0500

    Update Display.sql

--- Environmental Biology/DW - Student Missing Course/Display.sql
+++ Environmental Biology/DW - Student Missing Course/Display.sql
@@ -1834,7 +1834,6 @@ WHERE
     AND NOT EXISTS (SELECT * FROM rad_class_dtl@dgw13.dgw
                    WHERE   SUBSTR(TRIM(RAD_COURSE_KEY), 1,3) = :select_course_id.CRSE_ID
                    AND     SUBSTR(TRIM(RAD_COURSE_KEY), 13, 15) = :select_course_id.CRSE_NUMB
-                   --AND SGBSTDN_MAJR_CODE_1 in ('AFSC', 'ENBI', 'CONB', 'EEIN', 'FOHE', 'WLSC')
                    AND     SPRIDEN_ID = SUBSTR(RAD_ID, 1, 9))
                    AND     SFRSTCR_CRN = (SELECT MAX (SFRSTCR_CRN)
                                          FROM        SFRSTCR STCR
